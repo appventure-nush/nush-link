f0d850818dd1138e977fbf3d24f3d8bac1008dc0
frostyLegend <david.gohzhekai@gmail.com>
2020-11-17 22:31:17 +0800

    remove sequlize

--- server/src/config/database.ts
+++
@@ -1,7 +0,0 @@
-const DB_DATABASE = 'database';
-const DB_USERNAME = 'username';
-const DB_PASSWORD = 'password';
-const HOST = 'localhost';
-const DB_DIALECT = 'mysql';
-
-export { DB_DATABASE, DB_USERNAME, DB_PASSWORD, HOST, DB_DIALECT };
