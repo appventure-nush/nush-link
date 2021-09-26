5458592bda1060f30dceeb89692a8a16e28f174b
Lam Jun Rong <junron1@outlook.com>
2021-09-26 13:14:34 +0800

    Replace react frontend with vue base

---
+++ frontend/src/plugins/router.ts
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter, {RouteConfig} from "vue-router";
+import Main from "@/views/Main.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    component: Main,
+  },
+];
+
+export default new VueRouter({
+  routes,
+});
---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        Your profile
+        <v-spacer/>
+      </v-card-title>
+      <v-card-text>
+
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Main",
+  data: function (){
+    return {
+    };
+  },
+  methods:{
+  },
+});
+</script>
