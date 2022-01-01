54b7059c9028afbf67aee052f58d72524ae8660f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2022-01-01 14:26:39 +0800

    Enable Deletion

--- frontend/src/types/LinkData.ts
+++ frontend/src/types/LinkData.ts
@@ -1,8 +1,17 @@
-export interface LinkData {
+export class LinkData {
   id: number;
   url: string;
   creator: string;
   creatorEmail: string;
   createdOn: string;
-  alias: string
+  alias: string;
+
+  constructor(id: number, url: string, creator: string, creatorEmail: string, createdOn: string, alias: string) {
+    this.id = id;
+    this.url = url;
+    this.creator = creator;
+    this.creatorEmail = creatorEmail;
+    this.createdOn = createdOn;
+    this.alias = alias;
+  }
 }
