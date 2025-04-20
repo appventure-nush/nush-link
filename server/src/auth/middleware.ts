95e2f25be67a6a60a30cc330e08f7124834b29a1
Walnit <wongyueheng@gmail.com>
2025-04-20 23:26:19 +0800

    Allow async staff to create links

--- server/src/auth/middleware.ts
+++ server/src/auth/middleware.ts
@@ -37,7 +37,7 @@ export default () => async (
   if (decodedToken == null) return;
   req.email = decodedToken.unique_name;
   req.username = decodedToken.name;
-  const allowed = ["anhstjyl","anhsevxm", "anhs.chm", "anhsloh", "anhsczm", "anhsipoh", "shawnteo", "anhsaksy", "appventure", "nhs"];
+  const allowed = ["anhs", "appventure", "nhs"];
   console.log(req.email);
   if (!req.email.endsWith("@nushigh.edu.sg")){
     req.can_create_redirect = false;
